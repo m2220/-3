c25516bd4bdb6412d8ef79ce4fe1afbd2cef1a49
m2220 <101191167+m2220@users.noreply.github.com>
2022-04-16 00:48:49 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -16,14 +16,14 @@ async def on_message(message):
         return
 
 #embed:
-    if message.content.lower().strip().startswith('Jupiter'):
+    if message.content.lower().strip().startswith('.Jupiter'):
        embed_m = discord.Embed(colour=0x131010)
        embed_m.add_field(name="~",
                          value='\n\nhi, i love you.')
        embed_m.set_image(url="https://images.genius.com/02a227b51a5f263b3709ebc76c106200.1000x1000x1.jpg")                
        await message.channel.send(embed=embed_m)
 
-    if message.content.lower().strip().startswith('Back to You'):
+    if message.content.lower().strip().startswith('.Back to You'):
        embed_m = discord.Embed(colour=0x131010)
        embed_m.add_field(name="~",
                          value='\n\ni have an important question.')
@@ -34,22 +34,22 @@ async def on_message(message):
     elif message.content.lower().strip().startswith('.question'):
        await message.channel.send('https://open.spotify.com/track/7crSKtI09erdDYWLLsP4vi?si=22dddaea3eef4eac')
 
-    elif message.content.lower().strip().startswith('.question2'):
+    elif message.content.lower().strip().startswith('!question'):
        await message.channel.send('https://open.spotify.com/track/7hfi4ZTfV7akmGINh6qYCF?si=848c7ccdbe40411f')
 
-    elif message.content.lower().strip().startswith('.question3'):
+    elif message.content.lower().strip().startswith('@question'):
        await message.channel.send('https://open.spotify.com/track/1IxfE1rd0ngSFP8MSqYiv0?si=b9aa847b86cb4c43')
 
-    elif message.content.lower().strip().startswith('.question4'):
+    elif message.content.lower().strip().startswith('$question'):
        await message.channel.send('https://open.spotify.com/track/3U21A07gAloCc4P7J8rxcn?si=e9f656d468234c54')
 
-    elif message.content.lower().strip().startswith('.question5'):
+    elif message.content.lower().strip().startswith('*question'):
        await message.channel.send('https://open.spotify.com/track/3zWR0zS9p39c0FVkYkHfVF?si=4bebb8580afb480b')
 
-    elif message.content.lower().strip().startswith('.question6'):
+    elif message.content.lower().strip().startswith('^question'):
        await message.channel.send('https://open.spotify.com/track/3zWR0zS9p39c0FVkYkHfVF?si=4bebb8580afb480b')
 
-    elif message.content.lower().strip().startswith('.question7'):
+    elif message.content.lower().strip().startswith('#question'):
        await message.channel.send('https://open.spotify.com/track/63vr7fhaIB6Zq7YOJ1xqJm?si=b71f96b22bb348e0')
 
 
