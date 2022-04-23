85c9b36e196017c9c17dbcc8bb5df31ca98a57a0
m2220 <101191167+m2220@users.noreply.github.com>
2022-04-23 07:14:18 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -17,65 +17,22 @@ async def on_message(message):
 
 #embed:
 
-    elif message.content.lower().strip().startswith(',info'):
-       await message.channel.send('`react with .jupiter`')
+    elif message.content.lower().strip().startswith(''):
+       await message.channel.send('')
        time.sleep(2)
        await message.delete()
 
-    if message.content.lower().strip().startswith('.jupiter'):
+    if message.content.lower().strip().startswith(''):
        embed_m = discord.Embed(colour=0x89658d)
        embed_m.add_field(name="~",
-                         value='i love you.')
-       embed_m.set_image(url="https://media.discordapp.net/attachments/964663907463602206/964754029974532146/Screen_Shot_2022-04-16_at_1.08.00_AM.png")                
+                         value='')
+       embed_m.set_image(url="")                
        await message.channel.send(embed=embed_m)
        time.sleep(2)
        await message.delete()
 
 #commands:
-    elif message.content.lower().strip().startswith(',1'):
-       await message.channel.send('https://open.spotify.com/track/7crSKtI09erdDYWLLsP4vi?si=22dddaea3eef4eac')
-       time.sleep(2)
-       await message.delete()
 
-    elif message.content.lower().strip().startswith(',2'):
-       await message.channel.send('https://open.spotify.com/track/7hfi4ZTfV7akmGINh6qYCF?si=848c7ccdbe40411f')
-       time.sleep(2)
-       await message.delete()
-
-    elif message.content.lower().strip().startswith(',3'):
-       await message.channel.send('https://open.spotify.com/track/1IxfE1rd0ngSFP8MSqYiv0?si=b9aa847b86cb4c43')
-       time.sleep(2)
-       await message.delete()
-
-    elif message.content.lower().strip().startswith(',4'):
-       await message.channel.send('https://open.spotify.com/track/3U21A07gAloCc4P7J8rxcn?si=e9f656d468234c54')
-       time.sleep(2)
-       await message.delete()
-
-    elif message.content.lower().strip().startswith(',5'):
-       await message.channel.send('https://open.spotify.com/track/3zWR0zS9p39c0FVkYkHfVF?si=4bebb8580afb480b')
-       time.sleep(2)
-       await message.delete()
-
-    elif message.content.lower().strip().startswith(',6'):
-       await message.channel.send('you\'re aware im asking you out, yes?')
-       time.sleep(2)
-       await message.delete()
-
-    elif message.content.lower().strip().startswith(',proud'):
-       await message.channel.send('im just so proud of you. words can\'t describe.')
-       time.sleep(2)
-       await message.delete()
-
-    elif message.content.lower().strip().startswith(',other'):
-       await message.channel.send('I want to lay on the kitchen floor with you\n\nI want to do all the things that lovers do\n\nBruised on your face like a watercolour bloom\n\nMoonlight paints your skin... cornflower blue')
-       time.sleep(2)
-       await message.delete()
-
-    elif message.content.lower().strip().startswith(',melt'):
-       await message.channel.send('let it melt :))')
-       time.sleep(2)
-       await message.delete()
 
 
 
